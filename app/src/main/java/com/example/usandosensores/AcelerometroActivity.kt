30177b05ed89ffaa1117dadb2092441aac9e8041
FranBeal <francielebeal@gmail.com>
2025-10-14 20:14:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/usandosensores/AcelerometroActivity.kt
@@ -0,0 +1,76 @@
+package com.example.usandosensores
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
+
+class AcelerometroActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var tvX: TextView
+    private lateinit var tvY: TextView
+    private lateinit var tvZ: TextView
+    private lateinit var sensorManager: SensorManager
+    private var acelerometro: Sensor? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acelerometro)
+        tvX = findViewById(R.id.tvX)
+        tvY = findViewById(R.id.tvY)
+        tvZ = findViewById(R.id.tvZ)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE)
+        as SensorManager
+
+        acelerometro =
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        if(acelerometro == null){
+            Toast.makeText(this, "Acelerômetro não disponível",
+                Toast.LENGTH_LONG).show()
+            tvX.text = "Acelerômetro não encontrado"
+            tvY.text = ""
+            tvZ.text = ""
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            tvX.text = String.format(Locale.getDefault(),
+                "Eixo X: %.2f m/s²", x)
+            tvY.text = String.format(Locale.getDefault(),
+                "Eixo Y: %.2f m/s²", y)
+            tvZ.text = String.format(Locale.getDefault(),
+                "Eixo Z: %.2f m/s²", z)
+        }
+    }
+
+    override fun onResume(){
+        super.onResume()
+        sensorManager.registerListener(this,
+            acelerometro,
+            SensorManager.SENSOR_DELAY_UI)
+    }
+    override fun onPause(){
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+}
---
+++ app/src/main/java/com/example/usandosensores/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.usandosensores
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvListaSensores: TextView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var btDadosDoAcelerometro: Button
+    private lateinit var btMostrarLuminosidade: Button
+    private lateinit var btMostrarProximidade: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvListaSensores = findViewById(R.id.tvListaSensores)
+        btDadosDoAcelerometro = findViewById(R.id.btMostrarDadosAcelerômetro)
+        btMostrarLuminosidade = findViewById(R.id.btMostrarLuminosidade)
+        btMostrarProximidade = findViewById(R.id.btMostrarProximidade)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE)
+                as SensorManager
+
+        btDadosDoAcelerometro.setOnClickListener {
+            btAceleromentroOnClick()
+        }
+
+        btMostrarLuminosidade.setOnClickListener {
+            btLuminosidadeOnClick()
+        }
+
+        btMostrarProximidade.setOnClickListener {
+            btProxmidadeOnClick()
+        }
+
+        recuperaListaDeSensores()
+    }
+
+    private fun recuperaListaDeSensores() {
+        val sensorList: List<Sensor> =
+            sensorManager.getSensorList(Sensor.TYPE_ALL)
+        val sb = StringBuilder()
+        sb.append("Total de sensores: ${sensorList.size}\n\n")
+        sensorList.forEachIndexed{index, sensor ->
+            sb.append("${index+1}. Nome: ${sensor.name}\n")
+            sb.append("    Tipo: ${sensor.stringType}\n")
+            sb.append("    Fabricante: ${sensor.vendor}\n")
+            sb.append("----------------------------------\n")
+        }
+        tvListaSensores.text = sb.toString()
+    }
+
+    private fun btAceleromentroOnClick() {
+
+    }
+
+    private fun btProxmidadeOnClick() {
+
+    }
+
+    private fun btLuminosidadeOnClick() {
+
+    }
+}
