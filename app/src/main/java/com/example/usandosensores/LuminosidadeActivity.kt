2dea037e24a6df049bbcad2508d847651c57635d
FranBeal <francielebeal@gmail.com>
2025-10-15 21:24:38 -0300

    .

--- app/src/main/java/com/example/usandosensores/LuminosidadeActivity.kt
+++ app/src/main/java/com/example/usandosensores/LuminosidadeActivity.kt
@@ -1,20 +1,43 @@
 package com.example.usandosensores
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LuminosidadeActivity : AppCompatActivity() {
+class LuminosidadeActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var llTela: LinearLayout
+    private lateinit var sensorManager: SensorManager
+    private var lumi: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_luminosidade)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        llTela = findViewById(R.id.llTela)
+
+        sensorManager = getSystemService(
+            Context.SENSOR_SERVICE)
+                as SensorManager
+
+        lumi = sensorManager.getDefaultSensor(
+            Sensor.TYPE_LIGHT
+        )
+    }
+
+    override fun onAccuracyChanged(event: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
     }
 }
